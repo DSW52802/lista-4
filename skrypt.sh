a1e6916c6a1887999f9be58e8cf49f0c0f160ba1
DSW52802 <168664476+DSW52802@users.noreply.github.com>
2024-06-30 19:13:44 +0200

    dodanie --logi

--- skrypt.sh
+++ skrypt.sh
@@ -2,6 +2,14 @@
 
 if [ "$1" == "--date" ]; then
     date
+elif [ "$1" == "--logs" ]; then
+    num_files=${2:-100}
+    for ((i=1; i<=num_files; i++)); do
+        echo -e "Plik numer: $i\nNazwa skryptu: $0\nData: $(date)" > log_$i.txt
+    done
 else
-    echo "Użycie: $0 --date"
+    echo "Użycie: $0 --date | --logs [liczba]"
 fi
+
+
+
